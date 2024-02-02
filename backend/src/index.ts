110231747edeabd0e9e210c19e68c5a4a7c6a5d2
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-02-02 16:01:26 +0630

    backend

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+// src/index.js
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+// Other middleware
+app.use(morgan(`:date[clf] :method :url :status :response-time ms`));
+// Uncomment the cors middleware
+app.use(cors());
+
+// Set up body-parser middleware to parse application/x-www-form-urlencoded and application/json
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const mongoURL = "mongodb+srv://hlaingminthan:test1234@mern-cluster.cut3lbf.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongoURL).then(() => {
+    console.log('connected to db');
+    app.listen(process.env.PORT,() => {
+        console.log('app is running on localhost:'+process.env.PORT);
+    })
+});
+// app.use(helmet());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+// app.listen(port, () => {
+//   console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
