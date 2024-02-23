c8da8107ece63ca701dc6416d22eea8999adb6c2
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-02-23 14:28:51 +0630

    frontend config error fixed

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useReducer } from "react";
+
+import React from "react";
+
+let AuthContext = createContext();
+
+let AuthReducer = (state, action) => {
+  switch (action.type) {
+    case "LOG_IN":
+      return { ...state, user: action.payload };
+      break;
+    case "LOG_OUT":
+      return { ...state, user: action.payload };
+      break;
+    case "IS_AUTH_READY":
+      return { ...state, authReady: true };
+      break;
+    default:
+      return state;
+      break;
+  }
+};
+
+export default function AuthContextProvider({ children }) {
+  let [state, dispatch] = useReducer(AuthReducer, {
+    user: null,
+    authReady : false
+  });
+
+  useEffect(() => {
+
+      let userData = JSON.parse(localStorage.getItem("user"));
+      if (userData) {
+        dispatch({ type: "IS_AUTH_READY" });
+        dispatch({ type: "LOG_IN", payload: userData });
+      } else {
+        dispatch({ type: "LOG_OUT" });
+      }
+    
+  }, []);
+
+  return <AuthContext.Provider value={state}>{children}</AuthContext.Provider>;
+}
+
+export { AuthContext, AuthContextProvider };
---
+++ frontend/src/domain/user/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { AuthContext } from '../../../contexts/AuthContext'
+
+export default function Home() {
+  // let {user} =  useContext(AuthContext);
+  return (
+    <div className=' h-screen bg-slate-500'>
+      <h3>Home</h3>
+      <p>abcd</p>
+    </div>
+    
+  )
+}
