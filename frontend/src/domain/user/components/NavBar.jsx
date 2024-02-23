582a23813e9d2c85165e8f75ecd9bf31dae5585a
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-02-23 15:24:42 +0630

    nav design

--- frontend/src/domain/user/components/NavBar.jsx
+++ frontend/src/domain/user/components/NavBar.jsx
@@ -7,17 +7,28 @@ export default function NavBar() {
   return (
     <>
       <div className="w-full shadow-md">
-        <div className=" h-[60px] flex justify-between mx-10 items-center">
+        <div className=" h-[80px] flex justify-between mx-10 items-center">
           <p>Home</p>
           <div className=" w-full flex justify-center">
-            <input
-              type="text"
-              placeholder="search"
-              className="border w-1/2 p-1 rounded-xl"
-            />
+            <label className="input w-1/2 input-bordered flex items-center gap-2">
+              <input type="text" className="grow" placeholder="Search" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
           </div>
           <div className="flex items-center space-x-5">
-            <BtnGroup/>
+       
+            <BtnGroup />
           </div>
         </div>
       </div>
