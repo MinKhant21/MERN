6144a00b19b3be9f8a22ae7a0ddad1b5c1a41f98
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-04-01 16:49:04 +0630

    reactnative app initilization

---
+++ application/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+import React from 'react';
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-4xl">Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
